41cc20795d0794e61148553ff1ad4d4103fb5cf5
Anatoliy Valoviy <anatolia.valoviy@gmail.com>
2025-09-15 14:20:43 +0300

    last commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;
 
 /**
- * Клас, що представляє число Люка (варіант з початком 1, 3, 4, 7, 11...).
+ * Клас, що представляє число Люка.
  */
 class LucasNumber {
     private int index;
     private long value;
 
-    /**
-     * Конструктор для створення числа Люка.
-     * @param index порядковий номер числа (починаючи з 1)
-     * @param value значення числа Люка
-     */
     public LucasNumber(int index, long value) {
         this.index = index;
         this.value = value;
     }
 
-    /** @return порядковий номер числа */
     public int getIndex() {
         return index;
     }
 
-    /** @return значення числа Люка */
     public long getValue() {
         return value;
     }
 
-    /**
-     * Перевіряє, чи є число ідеальним квадратом.
-     * @return true, якщо число є квадратом
-     */
     public boolean isPerfectSquare() {
         long sqrt = (long) Math.sqrt(value);
         return sqrt * sqrt == value;
@@ -38,20 +27,16 @@ class LucasNumber {
 }
 
 /**
- * Головний клас програми для обчислення чисел Люка
- * та визначення, які з них є квадратами.
+ * Головний клас програми.
  */
 public class Main {
-    /**
-     * Обчислює n-те число Люка (початок 1, 3, 4, 7, 11...).
-     * @param n порядковий номер (починаючи з 1)
-     * @return значення числа Люка
-     */
-    public static long lucasCustom(int n) {
+
+    // Класичне обчислення числа Люка
+    public static long lucas(int n) {
+        if (n == 0) return 2;
         if (n == 1) return 1;
-        if (n == 2) return 3;
-        long a = 1, b = 3, c = 0;
-        for (int i = 3; i <= n; i++) {
+        long a = 2, b = 1, c = 0;
+        for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
@@ -71,16 +56,29 @@ public class Main {
 
         LucasNumber[] numbers = new LucasNumber[N];
 
-        // Заповнення масиву об'єктів
-        for (int i = 1; i <= N; i++) {
-            numbers[i - 1] = new LucasNumber(i, lucasCustom(i));
+        // Заповнення масиву
+        for (int i = 0; i < N; i++) {
+            numbers[i] = new LucasNumber(i, lucas(i));
         }
 
-        // Вивід результатів
-        System.out.println("Перші " + N + " чисел Люка (1, 3, 4, 7, 11...):");
+        // Вивід усіх чисел
+        System.out.println("Перші " + N + " чисел Люка (2, 1, 3, 4, 7...):");
         for (LucasNumber num : numbers) {
             String squareInfo = num.isPerfectSquare() ? " (квадрат)" : "";
             System.out.println("L_" + num.getIndex() + " = " + num.getValue() + squareInfo);
+        }
+
+        // Вивід лише квадратів
+        System.out.println("\nЧисла Люка, якi є квадратами:");
+        boolean found = false;
+        for (LucasNumber num : numbers) {
+            if (num.isPerfectSquare()) {
+                System.out.println("L_" + num.getIndex() + " = " + num.getValue());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Серед перших " + N + " чисел квадратів немає.");
         }
     }
 }
